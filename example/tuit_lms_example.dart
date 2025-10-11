2b626d638fafe9988832bc82a5b732483c38db5c
Isfandiyor Akbarov <isfandiyorakbarov450@gmail.com>
2025-10-11 13:13:44 +0500

    init

---
+++ example/tuit_lms_example.dart
@@ -0,0 +1,6 @@
+import 'package:tuit_lms/tuit_lms.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/tuit_lms_test.dart
@@ -0,0 +1,16 @@
+import 'package:tuit_lms/tuit_lms.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
