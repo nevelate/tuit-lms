bebb60a129d472ed54fd4a69b44394ff24b2440d
nevelate <isfandiyorakbarov450@gmail.com>
2026-02-14 00:15:55 +0500

    fix deleted file reference

--- lib/tuit_lms.dart
+++ lib/tuit_lms.dart
@@ -9,7 +9,6 @@ export 'src/languages.dart';
 export 'src/extensions.dart';
 export 'src/enums.dart';
 
-export 'src/core/cache_manager.dart';
 export 'src/core/tuit_lms_client.dart';
 
 // TODO: Export any libraries intended for clients of this package.
